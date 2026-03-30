52937b8ef6f3b0176878986c7814f3f29619026b
Tyson <tyson@tysonmorris.net>
2026-03-29 20:25:18 -0400

    updated background color

--- projects/generative-art-js-svg/01-hello-svg/hello-svg.js
+++ projects/generative-art-js-svg/01-hello-svg/hello-svg.js
@@ -1,8 +1,5 @@
 import { createSketch, random } from '/src/shared/sketch.js';
 
-// Create some global variables.
-const bgColor = '#121212';
-
 // Create an object to store some of our randomized parameters.
 const randomized = {
   hue: random(0, 360),
@@ -10,13 +7,7 @@ const randomized = {
   iterations: random(10, 100),
 };
 
-// Create our parent SVG and attach it to the element with id 'container'.
-// Set the width and height of the viewBox and the displayed size of the SVG.
 const { svg } = createSketch({ back: 'Generative Art with JavaScript and SVG' });
-
-// Create a background layer - a rectangle the full size of our viewBox.
-const rect = svg.create('rect');
-rect.set({ x: 0, y: 0, width: 1000, height: 1000, fill: bgColor });
 
 // Run a loop a random number of times to create our ellipses.
 for (let i = 0; i < randomized.iterations; i++) {
